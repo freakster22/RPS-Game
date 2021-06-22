a68a1f50591aaeab413fe1945e5b3eb8aeba1941
Freakster22 <hiareeb@gmail.com>
2021-06-22 21:11:14 +0530

    Added js File

---
+++ asset/main.js
@@ -0,0 +1,12 @@
+// //Write javaScript
+// when user clicks on any of the three buttons out of rock, paper scissors
+// on click of buttton, display the Image
+// random generator pulls out a Number out of 1,2,3 and at the digit specifies the Image for computer side, finally both are compared
+// 1-> rock
+// 2-> paper
+// 3-> scissors
+// if  2 beats 1
+//  3 beats 2
+//  1 beats 3
+//  as soon as the transition for user is done and both images are displayed
+//  use 3 var for win loss or tie
